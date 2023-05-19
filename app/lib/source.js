0a98229de65c7147a6ab0b789f92a6723044c255
danielluis07 <bombdaniel@hotmail.com>
2023-05-19 09:20:13 -0300

    tag

--- app/lib/source.js
+++ app/lib/source.js
@@ -29,3 +29,29 @@ fbq("track", "PageView");
     src="https://www.facebook.com/tr?id=1437872983710349&ev=PageView&noscript=1"
   />
 </noscript>;
+
+!(function (e) {
+  if (!window.pintrk) {
+    window.pintrk = function () {
+      window.pintrk.queue.push(Array.prototype.slice.call(arguments));
+    };
+    var n = window.pintrk;
+    (n.queue = []), (n.version = "3.0");
+    var t = document.createElement("script");
+    (t.async = !0), (t.src = e);
+    var r = document.getElementsByTagName("script")[0];
+    r.parentNode.insertBefore(t, r);
+  }
+})("https://s.pinimg.com/ct/core.js");
+pintrk("load", "2613661290541", { em: "<user_email_address>" });
+pintrk("page");
+
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style="display:none;"
+    alt=""
+    src="https://ct.pinterest.com/v3/?event=init&tid=2613661290541&pd[em]=<hashed_email_address>&noscript=1"
+  />
+</noscript>;
