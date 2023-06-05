e9797897a5d95ceac1efa0f28c14c2cebe5cee74
danielluis07 <bombdaniel@hotmail.com>
2023-06-05 20:54:39 -0300

    botão

--- app/sonodobebe/page.tsx
+++ app/sonodobebe/page.tsx
@@ -12,19 +12,18 @@ const Home = () => {
         <video src={"/sono.mp4"} controls />
         <div className="w-5/6 mx-auto py-10 text-center">
           <h1 className="sm:text-lg text-center font-bold">
-            Descubra os segredos para proporcionar noites inteiras de sono
-            tranquilo ao seu bebê
+            Pais exaustos: descubram o segredo para fazer seu bebê dormir a
+            noite toda
           </h1>
           <br />
           <h1 className="sm:text-lg text-center font-bold">
-            Aprenda estratégias comprovadas para estabelecer uma rotina de sono
-            saudável e eficiente para seu bebê
+            A solução definitiva para os problemas de sono do seu bebê.
+            Transforme noites agitadas em tranquilidade
           </h1>
           <br />
           <h1 className="sm:text-lg text-center font-bold">
-            Diga adeus as noites mal dormidas e transforme a rotina de sua
-            fofura, encontrando as melhores técnicas para garantir um sono
-            reparador sem interrupções
+            Diga adeus às noites em claro. Aprenda as técnicas comprovadas para
+            acalmar seu bebê e garantir seu sono profundo
           </h1>
         </div>
         <div className="flex justify-center">
@@ -34,7 +33,7 @@ const Home = () => {
             </a>
           </button>
         </div>
-        <div className="flex pt-28 pb-28 flex-row gap-x-8 justify-center items-center">
+        <div className="flex pt-28 pb-20 flex-row gap-x-8 justify-center items-center">
           <div className="w-[80px]">
             <Image src={visa} alt="visa" />
           </div>
@@ -46,6 +45,19 @@ const Home = () => {
           </div>
           <div className="w-[80px]">
             <Image src={boleto} alt="pix" />
+          </div>
+        </div>
+        <div className="flex w-5/6 mx-auto justify-center items-center">
+          <div className="flex bg-orange-300 hover:bg-orange-200 p-4 rounded-md justify-center flex-col gap-y-1">
+            <a href="https://pay.kiwify.com.br/JKceqDi" target="blank">
+              <p className="text-center cursor-pointer">R$36,50</p>
+            </a>
+            <a href="https://pay.kiwify.com.br/JKceqDi" target="blank">
+              <p className="text-center cursor-pointer">ou</p>
+            </a>
+            <a href="https://pay.kiwify.com.br/JKceqDi" target="blank">
+              <p className="text-center cursor-pointer">6x R$6,74</p>
+            </a>
           </div>
         </div>
         <div className="mt-20 bg-slate-100 py-16">
